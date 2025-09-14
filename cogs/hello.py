cec3e969dc69422af490c74f2c50cf2932f95fbe
Bartek Swatek <bmswatek@gmail.com>
2025-09-14 16:38:31 +0100

    Defined the testing command (hello) as a command instead of a prefix command

--- cogs/hello.py
+++ cogs/hello.py
@@ -1,17 +1,15 @@
-#Import commands extension from discord.py
-from discord.ext import commands 
+import discord
+from discord import app_commands
+from discord.ext import commands
 
-#Define a Cog (group of events and/or commands)
 class Hello(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    #Define a commands called "hello"
-    @commands.command()
-    async def hello(self, ctx): #ctx is Context of the command (who sent it, where etc.)
-        await ctx.send(f"Hello {ctx.author.mention}!") #If someone executes the command, they will receive a message saying "Hello {user}!"
+    #Define a slash command instead of a prefix command
+    @app_commands.command(name="hello", description="Say hello to the bot!")
+    async def hello(self, interaction: discord.Interaction):
+        await interaction.response.send_message(f"Hello {interaction.user.mention}!")
 
-#Function that makes the bot load this Cog
 async def setup(bot):
     await bot.add_cog(Hello(bot))
-    
